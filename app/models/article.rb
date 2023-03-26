cd44b174ac031fc2af9878c71f56ef205ac135ed
xieheng <xieheng0915@gmail.com>
2023-03-26 15:48:00 +0900

    reorganized study memo

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
   validates :title, presence: true, length: {minimum: 5, maximum:50}
   validates :description, presence: true
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_17_031747) do
+ActiveRecord::Schema.define(version: 2023_03_19_141846) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "students", force: :cascade do |t|
@@ -25,6 +26,12 @@ ActiveRecord::Schema.define(version: 2023_03_17_031747) do
     t.string "student_id"
     t.string "birthday"
     t.time "created_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
